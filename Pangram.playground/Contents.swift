a370d193876cd11745cb2410d40a381498484cf4
Parker Donat <parkerdonat@gmail.com>
2016-05-04 10:15:17 -0600

    Modified Pangram

--- Pangram.playground/Contents.swift
+++ Pangram.playground/Contents.swift
@@ -9,15 +9,41 @@ After typing the sentence several times, Roy became bored with it. So he started
     
 Given a sentence, tell Roy if it is a pangram or not.
  */
+import Foundation
 
 func isPangram(phrase: String) -> Bool {
+    let modifiedString = phrase.stringByReplacingOccurrencesOfString(" ", withString: "")
     let alphabet = "abcdefghijklmnopqrstuvwxyz"
     let alphabetSet = Set(alphabet.characters)
-    let phraseSet = Set(phrase.lowercaseString.characters)
-    let resultSet = alphabetSet.subtract(phraseSet)
+    let phraseSet = Set(modifiedString.lowercaseString.characters)
     
-    return resultSet.count == 0 ? true : false
+    let resultSet = alphabetSet.subtract(phraseSet)
+    let resultArray = Array(resultSet)
+    let sortedArray = resultArray.sort {String($0) < String($1)}
+    
+    if sortedArray.count == 0 {
+        print("pangram")
+        return true
+    } else {
+        print("not pangram")
+        return false
+    }
+    
+    //return sortedArray.count == 0 ? true : false
+}
+var pangram1 = ["We promptly judged antique ivory buckles for the next prize"]
+    
+var pangram2 = ["We promptly judged antique ivory buckles for the prize"]
+
+isPangram(pangram1[0])
+isPangram(pangram2[0])
+
+
+
+
+/// Another way
+func isAPangram(str: String) -> Bool {
+    let (char, alph) = (Set(str.characters), "abcdefghijklmnopqrstuvwxyz".characters)
+    return !alph.contains {!char.contains($0)}
 }
 
-isPangram("We promptly judged antique ivory buckles for the next prize")
-isPangram("We judged antique ivory buckles for the next prize")
