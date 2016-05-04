62902b955c11533eb06d1c3685175c399745b2fe
Parker Donat <parkerdonat@gmail.com>
2016-05-04 11:00:17 -0600

    Fixed some issues submitting to hackerrank

--- Pangram.playground/Contents.swift
+++ Pangram.playground/Contents.swift
@@ -11,7 +11,37 @@ Given a sentence, tell Roy if it is a pangram or not.
  */
 import Foundation
 
-func isPangram(phrase: String) -> Bool {
+///////////////// HackerRank Solution /////////////////////
+
+//func isPangram(phrase: String) -> Bool {
+//    
+//    let modifiedString = phrase.stringByReplacingOccurrencesOfString(" ", withString: "")
+//    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+//    let alphabetSet = Set(alphabet.characters)
+//    let phraseSet = Set(modifiedString.lowercaseString.characters)
+//    
+//    let resultSet = alphabetSet.subtract(phraseSet)
+//    let resultArray = Array(resultSet)
+//    let sortedArray = resultArray.sort {String($0) < String($1)}
+//    
+//    return sortedArray.count == 0 ? true : false
+//}
+//
+//
+//let sentence = readLine()!
+//
+//if isPangram(sentence) {
+//    print ("pangram")
+//}
+//else {
+//    print ("not pangram")
+//}
+
+
+
+////////////// MY SOLUTION ///////////////
+
+func isPangramer(phrase: String) -> Bool {
     let modifiedString = phrase.stringByReplacingOccurrencesOfString(" ", withString: "")
     let alphabet = "abcdefghijklmnopqrstuvwxyz"
     let alphabetSet = Set(alphabet.characters)
@@ -22,10 +52,8 @@ func isPangram(phrase: String) -> Bool {
     let sortedArray = resultArray.sort {String($0) < String($1)}
     
     if sortedArray.count == 0 {
-        print("pangram")
         return true
     } else {
-        print("not pangram")
         return false
     }
     
@@ -35,8 +63,8 @@ var pangram1 = ["We promptly judged antique ivory buckles for the next prize"]
     
 var pangram2 = ["We promptly judged antique ivory buckles for the prize"]
 
-isPangram(pangram1[0])
-isPangram(pangram2[0])
+isPangramer(pangram1[0])
+isPangramer(pangram2[0])
 
 
 
@@ -46,4 +74,35 @@ func isAPangram(str: String) -> Bool {
     let (char, alph) = (Set(str.characters), "abcdefghijklmnopqrstuvwxyz".characters)
     return !alph.contains {!char.contains($0)}
 }
+
+
+////////// PRACTICE /////////
+
+func isPangramerTest(phrase: String) -> Bool {
+    
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    let alphabetSet = Set(alphabet.characters)
+    let phraseSet = Set(phrase.lowercaseString.characters)
+    
+    let resultSet = alphabetSet.subtract(phraseSet)
+    
+    return resultSet.count == 0 ? true : false
+}
+var pangramTest1 = ["We promptly judged antique ivory buckles for the next prize"]
+
+var pangramTest2 = ["We promptly judged antique ivory buckles for the prize"]
+
+isPangramerTest(pangram1[0])
+isPangramerTest(pangram2[0])
+
+//let sentence = readLine()!
+
+//if isPangramerTest(sentence) {
+//    print ("pangram")
+//}
+//else {
+//    print ("not pangram")
+//}
+
+
 
